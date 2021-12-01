fee2e5a72d326668733818b8e0729ce6e92c8d69
Nassim Attemani <nassim.attem@hotmail.com>
2021-12-01 10:35:54 +0100

    add has_many

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,3 +1,4 @@
 class Course < ApplicationRecord
   belongs_to :family
+  has_many :progresses
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,4 @@
 class Question < ApplicationRecord
   belongs_to :family
+  has_many :answers
 end
