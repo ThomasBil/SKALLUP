18bc51ae460d7cf1c01e77d69778f17f7d16e19e
Nassim Attemani <nassim.attem@hotmail.com>
2021-12-02 15:29:46 +0100

    create for the quizz work, with the links

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,18 +1,49 @@
 class AnswersController < ApplicationController
   def create
     @answer = Answer.new(answer_params)
-    @question = Question.find(params[:id])
+    @question = Question.find(params[:question_id])
     @answer.question = @question
     @answer.user = current_user
-    raise
+    if @answer.user_answer == @question.correct_answer
+      @answer.status = true
+    else
+      @answer.status = false
+    end
+    @answer.save!
+    @quizz = @question.family
+    @quizz_questions = @quizz.questions
+    @quizz_questions.each do |quizz_question|
+      @next_question = quizz_question
+      break if quizz_question.answers.empty?
+    end
+    if @quizz_questions.last == @question
+      redirect_to quizzes_path
+    else
+      redirect_to question_path(@next_question)
+    end
   end
 
   def update
+    @answer = Answer.find(params[:id])
+    @new_answer = Answer.new(answer_params)
+    @question = Question.find(params[:question_id])
+    @answer.user_answer = @new_answer.user_answer
+    if @answer.user_answer == @question.correct_answer
+      @answer.status = true
+    else
+      @answer.status = false
+    end
+    @answer.save!
   end
 
   private
 
   def answer_params
-    params.require(:answer).permit(:user_answer, :question_id)
+    params.require(:answer).permit(:user_answer)
   end
 end
+
+
+
+
+# offers = Offer.where("income > :value", {value: 3})
--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,11 +1,13 @@
 class QuestionsController < ApplicationController
   def show
     @question = Question.find(params[:id])
-  end
-
-  private
-
-  def question_params
-    params.require(:answer).permit(:user_answer, :question_id)
+    @user = current_user
+    @user_answers = @user.answers
+    @user_answers.each do |user_answer|
+      if user_answer.question == @question
+        @status = true
+        @user_answer = user_answer
+      end
+    end
   end
 end
